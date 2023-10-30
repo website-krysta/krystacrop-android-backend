0664a382acd0200d104ffcd6b6045a4e7f59a619
website-krysta <website.krysta@gmail.com>
2023-10-30 17:35:15 +0530

    sucessfullu loging customize

--- krystabackend/app/views.py
+++ krystabackend/app/views.py
@@ -12,13 +12,30 @@ from .serializers import UserSerializer
 current_date = datetime.datetime.now().date()
 current_date_time = datetime.datetime.now()
 
-@api_view(['GET'])
+@api_view(['POST'])
 def UserList(request):
-    if request.method == 'GET':
-        queryset = user.objects.filter(UserStatus=True)
-        serializer_data = UserSerializer(queryset ,many=True)
-        return Response(serializer_data.data)
-    
+    if request.method == 'POST':
+        queryset = user.objects.all().values()
+        useriem = user.objects.get(EmailID=request.data['EmailID'])
+        user_details = {  
+                'UserID':0,	
+                'EmailID' :'',	
+                'Password'  :'',	
+                'Role':'',	
+                'UserStatus':'',		
+        }
+        user_details['UserID'] = useriem.UserID
+        user_details['EmailID'] = useriem.EmailID
+        user_details['Password'] = useriem.Password
+        user_details['Role'] = useriem.Role
+        user_details['UserStatus'] = useriem.UserStatus
+
+        # userdata = UserSerializer(data = user_details)
+        serializer_data = UserSerializer(instance=useriem ,data = user_details)
+        if serializer_data.is_valid():
+            for fields in queryset:
+                if fields['EmailID'] ==  request.data['EmailID']and fields['Password'] == request.data['Password']:
+                        return Response(serializer_data.data, status=status.HTTP_200_OK)
 # @api_view(['POST'])
 # def addmaterial(request):
 #     if request.method == 'POST':
